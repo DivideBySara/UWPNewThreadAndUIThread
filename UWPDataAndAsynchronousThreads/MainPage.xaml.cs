ab0ebfb0faafeb8e5ab6f589e3042b6d89935002
Sara Jade <v-sajade@microsoft.com>
2018-01-10 22:43:11 -0800

    Add demo for accessing the UI thread from the new thread

--- UWPDataAndAsynchronousThreads/MainPage.xaml.cs
+++ UWPDataAndAsynchronousThreads/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,5 +27,77 @@ namespace UWPDataAndAsynchronousThreads
         {
             this.InitializeComponent();
         }
-    }
-}
+
+        private async void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            var primeList = new List<int>();
+
+            int potentialPrimeNum = 0;
+            int maxNumTested = 500000000;
+
+            // **** Demo a new thread completely separate from the UI thread ***
+            //await Task.Run(() =>
+            //{
+            //    for (int counter = 0; counter < maxNumTested; ++counter)
+            //    {
+            //        if (IsPrime(potentialPrimeNum))
+            //        {
+            //            primeList.Add(potentialPrimeNum);
+            //        }
+            //    }
+            //});
+
+            //result.Text = $"All primes up to {maxNumTested} found!"; // must update UI on the UI thread, not the thread we just created.
+
+            // *** Demo accessing the UI thread from the new thread ***
+            await Task.Run( async () =>
+            {
+                for (int counter = 0; counter < maxNumTested; ++counter)
+                {
+                    if (IsPrime(potentialPrimeNum))
+                    {
+                        primeList.Add(potentialPrimeNum);
+                    }
+                }
+
+                // how to access the UI thread 
+                await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                    result.Text = $"All primes up to {maxNumTested} found!"; // must update UI on the UI thread, not the thread we just created.
+                });
+            });            
+        } // End btn1_Click()
+
+        private bool IsPrime(int x)
+        {
+            if (x <= 0)
+            {
+                return false;
+            }
+
+            if (x <= 3) // We've already taken care of cases <= 0
+            {
+                return true;
+            }
+
+            if (x % 2 == 0 || x % 3 == 0) // if x is divisible by 2 or 3
+            {
+                return false;
+            }
+
+            int i = 5;
+            while (i * i < x)
+            {
+                // if x is divisible by 5 or 7.
+                // in subsequent iterations, if x is divisible by 5 or 7 plus a multiple of 6
+                if (x % i == 0 || x % (i + 2) == 0) 
+                {
+                    return false;
+                }
+
+                i += 6;
+            }
+
+            return true; // if code reaches this point, x is prime
+        } // End IsPrime()
+    } // End parital class MainPage
+} // End namespace
